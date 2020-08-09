6431ffb16792ea09a4ccd8f1b867ef07238ebf69
Jamil Matheny <36749450+jamilimages@users.noreply.github.com>
2020-08-09 11:22:02 -0400

    Update BMIIndex.java

--- BMIIndex.java
+++ BMIIndex.java
@@ -14,28 +14,30 @@ public class BMIIndex {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		// Title of the Program
+		System.out.println("||||| Body Mass Index | Calculator ||||");
+		System.out.println("_______________________________________");
+		System.out.println("");
 
-
-		System.out.println("Enter the user's height in inches: ");
-		// Input
+		// Prompt for user input to collect data
+		System.out.println("Enter the user's height in inches: ")
 		Scanner input = new Scanner(System.in);
 
 		double heightInches = input.nextInt();
-
 		System.out.println("Enter the user's weight in Pounds: ");
-
+		
+		// Processes and calculates the functionality per metric
+		// This function will require all other double "variables (up top)" to calculate the math 
 		double weightPounds = input.nextInt();
-
 		double heightMeters = heightInches * 2.54/100;
 		double weightKilos = weightPounds * 453.59/1000;
 		double bmi = weightKilos/(heightMeters * heightMeters);
 
-		// Output
-
+		// Outputs the BMI result here
 		System.out.println("Your BMI is: " + bmi);
 
 		// Close program
-
 		input.close();
 
 	}
